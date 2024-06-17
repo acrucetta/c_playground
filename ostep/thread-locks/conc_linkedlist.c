da19a70f17f6c2230d23ba8192ca35cb53757e78
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-06-17 10:12:10 -0500

    adding time checks for the concurrent linked list

--- ostep/thread-locks/conc_linkedlist.c
+++ ostep/thread-locks/conc_linkedlist.c
@@ -1,36 +1,56 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+#define LIST_LENGTH 1000
 
 // Concurrent Linked List
 
-typedef struct __node_t {
+typedef struct __node_t
+{
   int key;
   pthread_mutex_t lock;
   struct __node_t *next;
 } node_t;
 
-typedef struct __list_t {
+typedef struct __list_t
+{
   node_t *head;
   pthread_mutex_t lock;
 } list_t;
 
-void List_Init(list_t *L) {
+void List_Init(list_t *L)
+{
   L->head = NULL;
   pthread_mutex_init(&L->lock, NULL);
 }
 
-int List_Insert(list_t *L, int key) {
+int List_Insert(list_t *L, int key)
+{
   node_t *new = malloc(sizeof(node_t));
-  if (new == NULL) {
+  if (new == NULL)
+  {
     perror("malloc");
     return -1;
   }
   new->key = key;
+  new->next = NULL;
 
-  node_t *prev, *curr;
 
   pthread_mutex_lock(&L->lock);
+
+  // If this is the first insert, head will be NULL
+  // so we want to insert at the beginning of the list
+  if (L->head == NULL || L->head->key > key)
+  {
+    new->next = L->head;
+    L->head = new;
+    pthread_mutex_unlock(&L->lock);
+    return 0;
+  }
+
+  node_t *prev, *curr;
   prev = L->head;
   curr = L->head->next;
 
@@ -39,7 +59,8 @@ int List_Insert(list_t *L, int key) {
   if (curr)
     pthread_mutex_lock(&curr->lock);
 
-  while (curr) {
+  while (curr)
+  {
     if (curr->key > key)
       break;
     node_t *old_prev = prev;
@@ -57,7 +78,8 @@ int List_Insert(list_t *L, int key) {
     pthread_mutex_unlock(&curr->lock);
 }
 
-void List_Delete(list_t *L, int key) {
+void List_Delete(list_t *L, int key)
+{
   // Grab prev and head
   node_t *prev, *curr;
   pthread_mutex_lock(&L->lock);
@@ -69,8 +91,10 @@ void List_Delete(list_t *L, int key) {
     pthread_mutex_lock(&curr->lock);
 
   // Iterate until we find the desired key to delete
-  while (curr) {
-    if (curr->key == key) { // Found the value
+  while (curr)
+  {
+    if (curr->key == key)
+    { // Found the value
       prev->next = curr->next;
       pthread_mutex_unlock(&prev->lock);
       pthread_mutex_unlock(&curr->lock);
@@ -88,15 +112,17 @@ void List_Delete(list_t *L, int key) {
   pthread_mutex_unlock(&prev->lock);
 }
 
-int List_Lookup(list_t *L, int key) {
+int List_Lookup(list_t *L, int key)
+{
   int rv = -1;
   pthread_mutex_lock(&L->lock);
   node_t *curr = L->head;
-  while (curr) {
-    printf("Iterating through node key: %i", curr->key);
-    if (curr->key == key) {
+  while (curr)
+  {
+    if (curr->key == key)
+    {
       rv = 0;
-      printf("Found value\n");
+      printf("Found value: %i\n",key);
       break;
     }
     curr = curr->next;
@@ -107,9 +133,11 @@ int List_Lookup(list_t *L, int key) {
 
 // Concurrent Operations
 
-void *thread_func(void *arg) {
+void *thread_func(void *arg)
+{
   list_t *list = (list_t *)arg;
-  List_Lookup(list,0);
+  int number = (random() / ((double) RAND_MAX + 1)) * LIST_LENGTH;
+  List_Lookup(list, number);
   return NULL;
 }
 
@@ -117,31 +145,45 @@ void *thread_func(void *arg) {
 Goal:
 - Use over hand locking and test the performance at different
 thread values
-- 
+  - Insert multiple values at a time
+  - Lookup the same key simultaneously
 */
-int main(int argc, char *argv) {
-    if (argc<2) {
-        fprintf(stderr, "Usage counter <threads>");
-        exit(1);
-    }
 
-    struct timeval start, end;
-    
-    int N_THREADS = atoi(argv[1]);
-    pthread_t p[N_THREADS];
-    gettimeofday(&start, NULL);
-    for (size_t i = 0; i < N_THREADS; i++)
-    {
-        pthread_create(&p[i], NULL, &increase_counter, &counter);
-    }
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    fprintf(stderr, "Usage counter <threads>");
+    exit(1);
+  }
 
-    for (size_t i = 0; i < N_THREADS; i++)
-    {
-        pthread_join(p[i], NULL);
-    }
-    gettimeofday(&end, NULL);
+  struct timeval start, end;
+  int N_THREADS = atoi(argv[1]);
+  list_t L;
+  List_Init(&L);
 
-    long seconds = end.tv_sec - start.tv_sec;
-    long micros = end.tv_usec- start.tv_usec;
-    long elapsed = seconds * 1000000 + micros;
+  // Add artificial values
+  for (size_t i = 0; i < LIST_LENGTH; i++)
+  {
+    List_Insert(&L, i);
+  }
+
+  pthread_t p[N_THREADS];
+  gettimeofday(&start, NULL);
+  for (size_t i = 0; i < N_THREADS; i++)
+  {
+    pthread_create(&p[i], NULL, &thread_func, &L);
+  }
+
+  for (size_t i = 0; i < N_THREADS; i++)
+  {
+    pthread_join(p[i], NULL);
+  }
+  gettimeofday(&end, NULL);
+
+  long seconds = end.tv_sec - start.tv_sec;
+  long micros = end.tv_usec - start.tv_usec;
+  long elapsed = seconds * 1000000 + micros;
+
+  printf("Elapsed Time: %ld (ms)", elapsed);
 }
