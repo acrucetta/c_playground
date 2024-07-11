82600c43f92f98660ebfdf20ed60d90a54ecd880
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-07-11 10:02:18 -0500

    kqeueue server is now working

--- networks/client.c
+++ networks/client.c
@@ -1,52 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/un.h>
-#include <errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <netinet/in.h>
 
-#define SOCKET_PATH "unix_socket"
+#define SERVER_PORT 1815
+#define BUFFER_SIZE 1024
 
 int main() {
-    int client_socket;
-    struct sockaddr_un server_addr;
-    char send_char = 'C';
-    char recv_char;
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE] = {0};
+    char *message = "Hello from client!";
 
     // Create socket
-    client_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        perror("socket");
-        exit(1);
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
     }
 
-    // Prepare the sockaddr_un structure
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sun_family = AF_UNIX;
-    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(SERVER_PORT);
+
+    // Convert IPv4 address from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        return -1;
+    }
 
     // Connect to server
-    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("connect");
-        exit(1);
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        return -1;
     }
 
-    // Send data
-    if (write(client_socket, &send_char, 1) != 1) {
-        perror("write");
-        exit(1);
+    printf("Connected to server. Sending message...\n");
+
+    // Send message to server
+    send(sock, message, strlen(message), 0);
+    printf("Message sent to server: %s\n", message);
+
+    // Receive response from server
+    int valread = read(sock, buffer, BUFFER_SIZE);
+    if (valread > 0) {
+        printf("Server response: %s\n", buffer);
+    } else if (valread == 0) {
+        printf("Server closed the connection\n");
+    } else {
+        perror("Read error");
     }
 
-    // Receive data
-    if (read(client_socket, &recv_char, 1) != 1) {
-        perror("read");
-        exit(1);
-    }
-
-    printf("Client: I received %c from server!\n", recv_char);
-
-    close(client_socket);
+    close(sock);
     return 0;
 }
